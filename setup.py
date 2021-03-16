97b36a1e95b1c182d9ca3106325f5b2d20eea720
marcinadd <marcinadd@gmail.com>
2021-03-16 11:28:32 +0100

    Add paho-mqtt as required dependency to requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.0.1',
     description='ThingooConnector',
     author='KNI',
-    install_requires=['requests'],
+    install_requires=['requests', 'paho-mqtt'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
